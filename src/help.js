c9556b7b809f396d948ece930346cf577e0db2fd
Elbielzinho <59930738+Elbielzinho@users.noreply.github.com>
2021-04-27 20:00:49 -0200

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,6 +1,98 @@
 const help = (prefix) => {
-	return `> *Comandos* <
---» : *${prefix}sticker*`
+	return `*>* 🇵 🇴 🇸 🇪 🇮 🇩 🇴 🇳 *<*
+
+
+*_${prefix}sttc (texto)_*
+*Info:* _Faça figurinhas animadas com texto/emoji._
+
+*_${prefix}gtts (idioma) <texto>_*
+*Info:* _Faça um texto virar áudio._
+
+*_${prefix}ping_*
+*Info:* _Veja os dados do bot._
+~Exemplo: ${prefix}gtts pt Olá, amigo.~
+
+*_${prefix}ocr_*
+*Info* _Copie o texto que está na imagem._
+
+*_${prefix}criador_*
+*Info:* _Veja quem criou esse Bot._
+
+*_${prefix}wame_*
+*Info:* _Crie o seu link de WhatsApp_
+
+*_${prefix}play (música)_*
+*Info:* _Baixe uma música em MP3_
+
+*_${prefix}porcentagem (pergunta)_*
+*_${prefix}porcentagem_*
+*Info:* _Tire uma porcentagem de uma certa coisa._
+
+*_${prefix}perguntar (pergunta)_*
+*Info:* _Faça perguntas para o bot._
+
+*_${prefix}amor_*
+*Info:* _Veja frases de amor._
+
+*_${prefix}cantadas_*
+*Info:* _Veja as melhores cantadas e use com a pessoa do seu coração._
+
+*_${prefix}cassino_*
+*Info:* _Jogue no cassino_
+
+*_${prefix}qrcode (texto)_*
+*Info:* _Faça palavras/emoji virar um codigo QR._
+
+*_${prefix}wait_*
+*Info:* _Coloque o wait em uma legenda de um anime para pesquiaar ao respeito dele._
+
+*>* 🇬 🇷 🇺 🇵 🇴 🇸 *<*
+
+*${prefix}casal*
+*nfo:* _Faça que o bot shippa alguem_
+
+*${prefix}corno @tag*
+*Info:* _Veja quantos de porcento que você ou o seu amigo é corno._
+
+*${prefix}rankcorno*
+*Info:* _Veja os top5 corno._
+
+*${prefix}lgbt*
+*Info:* _Veja quantos de porcento que você ou o seu amigo é lgbt._
+
+*${prefix}ranklgbt*
+*Info* _Veja os top5 LGBT_
+
+*${prefix}ajuda*
+*Info:* _Marque todos os ajudantes do grupo._
+
+*${prefix}lista ou ${prefix}lista(2/3)*
+*Info:* _Marque todos os membros do grupo._
+
+*${prefix}op @tagmembro*
+*Info:* _Dar admin para um membro._
+
+*${prefix}deop @tagadmin*
+*Info:* _Remove admin de um administrador._
+
+*${prefix}ban @tagmembro*
+*Info:* _Remove um membro do grupo._
+
+*${prefix}link*
+*Info:* _Crie um link de convite desse grupo._
+
+*${prefix}welcome 1/0*
+*Info:* _1 para ativar o sistema de boas vindas e 0 para desativar._
+
+*${prefix}antilink 1/0*
+*Info:* _1 para ativar o sistema de antilink e 0 para desativar._
+
+*${prefix}antidominio 1/0*
+*Info:* _Ajudante do antilink_
+
+*${prefix}antixingamento 1/0*
+*Info:* _Ban todos que usar palavras relacionada a desrespeito um com outro._
+`
 }
 
 exports.help = help
